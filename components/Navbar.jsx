f5040abdffec92f25f147973a8119338576c91cd
Vlad Chepurnyi <chepurnojv@gmail.com>
2024-03-29 15:49:12 +0200

    homepage comp., properties page, updated property card dynamic data

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -214,15 +214,16 @@ const Navbar = () => {
 							</Link>
 						)}
 
-						<button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4">
-							<FaGoogle className="text-white mr-2" />
-							<span>Login or Register</span>
-						</button>
+						{!isLoggedIn && (
+							<button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4">
+								<span>Login or Register</span>
+							</button>
+						)}
 					</div>
 				</div>
 			)}
 		</nav>
 	);
 };
-
+// <FaGoogle className="text-white mr-2" />
 export default Navbar;
