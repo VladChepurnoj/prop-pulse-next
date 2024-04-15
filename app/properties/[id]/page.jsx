3b6d97383f2d75a7186b2fe48b5b18d8bea39fb8
Vlad Chepurnyi <chepurnojv@gmail.com>
2024-04-15 11:48:41 +0300

    property image to display, user profile info

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { fetchProperty } from "@/utils/requests";
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
 import PropertyDetails from "@/components/PropertyDetails";
+import PropertyImages from "@/components/PropertyImages";
 import Spinner from "@/components/Spinner";
 import { FaArrowLeft } from "react-icons/fa";
 
@@ -138,6 +139,7 @@ const PropertyPage = () => {
 							</div>
 						</div>
 					</section>
+					<PropertyImages images={property.images} />
 				</>
 			)}
 		</>
