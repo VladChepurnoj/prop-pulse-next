6554d19ff016d96b8a6a93398ed419531d718838
Vlad Chepurnyi <chepurnojv@gmail.com>
2024-04-24 12:43:49 +0300

    bookmark status & button toggle

--- app/api/bookmarks/route.js
+++ app/api/bookmarks/route.js
@@ -5,6 +5,32 @@ import { getSessionUser } from "@/utils/getSessionUser";
 
 export const dynamic = "force-dynamic";
 
+// GET /api/bookmarks
+export const GET = async () => {
+	try {
+		await connectDB();
+
+		const sessionUser = await getSessionUser();
+
+		if (!sessionUser || !sessionUser.userId) {
+			return new Response("User id is required", { status: 401 });
+		}
+
+		const { userId } = sessionUser;
+
+		//find user in db
+		const user = await User.findOne({ _id: userId });
+
+		//get users bookmarks
+		const bookmarks = await Property.find({ _id: { $in: user.bookmarks } });
+
+		return new Response(JSON.stringify(bookmarks), { status: 200 });
+	} catch (error) {
+		console.log(error);
+		return new Response("Something went wrong", { status: 500 });
+	}
+};
+
 export const POST = async (request) => {
 	try {
 		await connectDB();
@@ -14,7 +40,7 @@ export const POST = async (request) => {
 		const sessionUser = await getSessionUser();
 
 		if (!sessionUser || !sessionUser.userId) {
-			return new Response("User if is required", { status: 401 });
+			return new Response("User id is required", { status: 401 });
 		}
 
 		const { userId } = sessionUser;
@@ -34,7 +60,7 @@ export const POST = async (request) => {
 			isBookmarked = false;
 		} else {
 			//if not bookmarked add it
-			user.bookmarks.push(propertyID);
+			user.bookmarks.push(propertyId);
 			message = "Bookmark added successfully";
 			isBookmarked = true;
 		}
